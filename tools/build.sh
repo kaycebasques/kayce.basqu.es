67825346cc20bd092f5af713e44b9838822d2cf2
Kayce Basques <kaycebasques@gmail.com>
2018-03-20 16:25:46 +0100

    copy images to build dir

--- tools/build.sh
+++ tools/build.sh
@@ -3,8 +3,6 @@ SCRIPT="tools/build.js";
 if [ -d 'build' ]; then rm -rf build; fi;
 mkdir build;
 mkdir build/blog;
-if [ "$DEBUG" = true ]; then
-  node --inspect-brk $SCRIPT;
-else
-  node $SCRIPT;
-fi
+mkdir build/images;
+cp images/* build/images/;
+if [ "$DEBUG" = true ]; then node --inspect-brk $SCRIPT; else node $SCRIPT; fi;
