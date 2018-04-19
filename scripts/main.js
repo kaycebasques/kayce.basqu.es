4ae54c0e160dfd8c09e6893df3e0a4c2225be952
Kayce Basques <kaycebasques@gmail.com>
2018-04-18 18:57:37 -0700

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,1 @@
-function superLongFunctionName(argumentA, argumentB) {
-  // My favorite color is blue.
-  console.log(argumentA, argumentB);
-}
-superLongFunctionName('cat', 'dog');
+console.log('cat dog');
