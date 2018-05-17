65699342f1a6af8731190642920ba15ccd28e769
Kayce Basques <kaycebasques@gmail.com>
2018-05-17 10:59:12 -0700

    add regression tests post

--- tools/build.sh
+++ tools/build.sh
@@ -3,6 +3,6 @@ SCRIPT="tools/build.js";
 if [ -d 'build' ]; then rm -rf build; fi;
 mkdir build;
 mkdir build/on;
-mkdir build/images;
-cp images/* build/images/;
+mkdir build/media;
+cp media/* build/media/;
 if [ "$DEBUG" = true ]; then node --inspect-brk $SCRIPT; else node $SCRIPT; fi;
