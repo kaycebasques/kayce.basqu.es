c2a369a4e395c1352894d28a9342f102dbef38b1
Kayce Basques <kaycebasques@gmail.com>
2018-09-02 12:26:10 -0700

    support the font directory

--- tools/build.sh
+++ tools/build.sh
@@ -4,5 +4,7 @@ if [ -d 'build' ]; then rm -rf build; fi;
 mkdir build;
 mkdir build/on;
 mkdir build/media;
+mkdir build/fonts;
 cp media/* build/media/;
+cp -r fonts/* build/fonts/;
 if [ "$DEBUG" = true ]; then node --inspect-brk $SCRIPT; else node $SCRIPT; fi;
