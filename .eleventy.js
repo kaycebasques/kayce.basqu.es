0fd58a97815aee85270e71e576b744d3ed36130e
Kayce Basques <kaycebasques@gmail.com>
2020-01-10 16:43:12 -0800

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('media');
+  // https://github.com/11ty/eleventy-base-blog/commit/096ff9f3deeab3b393a496a202f1353f16f81572
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+  });
   return {
     dir: {
       input: 'content',
@@ -7,6 +13,7 @@ module.exports = function(eleventyConfig) {
     templateFormats: [
       'md',
       'css',
+      'njk'
     ],
     markdownTemplateEngine: 'njk',
   };
