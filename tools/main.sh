6844379a55c633abea63eca173025311b80e679a
Kayce Basques <kayce@google.com>
2021-01-12 14:04:25 -0800

    delete old stuff

--- tools/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT="tools/build.js";
-if [ -d 'out' ]; then rm -rf out; fi;
-mkdir out;
-mkdir out/blog;
-mkdir out/portfolio;
-mkdir out/media;
-cp media/* out/media/;
-if [ "$DEBUG" = true ]; then node --inspect-brk $SCRIPT; else node $SCRIPT; fi;
--- tools/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'),
-    app = express(),
-    PORT = 4040;
-
-app.use(express.static(`${__dirname}/../out`));
-
-app.listen(PORT, () => {
-  console.log(`App started on port ${PORT}.`);
-});
