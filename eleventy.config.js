44b52ee34a45aad4c889b3accae4e41e8b803395
Kayce Basques <kayce@google.com>
2024-07-12 07:58:14 -0700

    Use Atkinson font

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = config => {
   config.addPassthroughCopy('src/**/*.css');
   config.addPassthroughCopy('src/**/*.jpg');
   config.addPassthroughCopy('src/**/*.pdf');
+  config.addPassthroughCopy('src/**/*.woff2');
   return {
     dir: {
       input: 'src',
