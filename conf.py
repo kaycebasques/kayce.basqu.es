753e62a674c0370e894c7f8ce017cda61775f15e
Kayce Basques <kayce@google.com>
2025-01-20 22:43:27 -0800

    Add books extension

--- conf.py
+++ conf.py
@@ -1,18 +1,36 @@
+import sys
+import pathlib
+
+
+sys.path.append(str(pathlib.Path('_extensions').resolve()))
+
+
+project = 'kayce.basqu.es'
 author = 'Kayce Basques'
 copyright = '2025, Kayce Basques'
+release = '0.0.1'
+
+
 exclude_patterns = [
     '.github',
     '.gitignore',
     '_build',
+    '_extensions',
     'boostrap.fish',
     'requirements.txt',
     'venv'
 ]
-extensions = []
+templates_path = ['_templates']
+
+
+extensions = [
+    'books'
+]
+
+
 html_extra_path = []
 html_permalinks_icon = '#'
 html_static_path = ['_static']
-project = 'kayce.basqu.es'
+
+
 pygments_style = 'sphinx'
-release = '0.0.1'
-templates_path = ['_templates']
--- index.rst
+++ index.rst
@@ -3,3 +3,11 @@ kayce.basqu.es
 ==============
 
 Hello! My name is Kayce. This is my website.
+
+-----
+Books
+-----
+
+Books I'm currently reading:
+
+.. books::
